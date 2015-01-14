1c0648869f880353e71652975e3a923e2aad6671
Mihai Baboi <mihail.baboi@gmail.com>
2015-01-14 21:14:07 +0000

    Set up devise for user registration and authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mandrillapp.com',
+    port: 587,
+    enable_starttls_auto: true,
+    user_name: 'mihail.baboi@gmail.com',
+    password: 'PnqwUWDSdPr6XkE5j2E5cw',
+    authentication: 'login'
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default charset: 'utf-8'
 end
