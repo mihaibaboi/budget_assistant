750bc27cb8780b55d913b2e65306c3a022f41901
Mihai Baboi <mihail.baboi@gmail.com>
2015-01-30 10:53:46 +0000

    Updated `User` model and mode tests to accomodate the `ActiveRelation` with `Account` model

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    email 'factory_girl@budget-assistant.com'
+    password 'testpass21'
+    confirmed_at '2015-01-21'
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should respond to active relations' do
+    user = create(:user)
+    
+    assert_respond_to(user, :accounts)
+  end
 end
