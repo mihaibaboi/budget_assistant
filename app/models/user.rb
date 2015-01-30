b61f33945f9210ae644dc5018b00930b47a96a72
Mihai Baboi <mihail.baboi@gmail.com>
2015-01-30 15:19:21 +0000

    Made sure that destroying a `:user` doesn't leave orphaned records in the `accounts` table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable, :confirmable
   
-  has_many :accounts
+  has_many :accounts, dependent: :destroy # :restrict_with_error is also available
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,12 @@ class UserTest < ActiveSupport::TestCase
     
     assert_respond_to(user, :accounts)
   end
+  
+  test 'should respect dependent tables' do
+    user = create(:user)
+    create(:account, user_id: user.id)
+    
+    assert(user.destroy)
+    assert_equal(0, Account.all.count)
+  end
 end
