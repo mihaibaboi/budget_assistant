de7ee40d2b88b2c710e6cd831cdf10468cf41578
Mihai Baboi <mihail.baboi@gmail.com>
2015-02-04 10:54:17 +0000

    Updated model and unit tests with `:user` -> `:category` ActiveRelation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,13 +5,16 @@ class UserTest < ActiveSupport::TestCase
     user = create(:user)
     
     assert_respond_to(user, :accounts)
+    assert_respond_to(user, :categories)
   end
   
   test 'should respect dependent tables' do
     user = create(:user)
     create(:account, user_id: user.id)
+    create(:category, user_id: user.id)
     
     assert(user.destroy)
     assert_equal(0, Account.all.count)
+    assert_equal(0, Category.all.count)
   end
 end
